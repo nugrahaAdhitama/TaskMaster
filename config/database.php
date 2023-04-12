2507aaaae36f28a521e979f459cd058dbad11108
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-04-13 06:44:11 +0700

    Add database configuration file
    This commit introduces the 'config/database.php' file, which sets up the database connection for the Taskmaster application. The file contains the necessary configuration options, such as the database host, username, password, and database name, to establish a connection using the PDO (PHP Data Objects) extension.
    
    With the inclusion of this file, the application now has a centralized location for managing database connection settings, making it easier to update and maintain in the future.

--- config/database.php
+++ config/database.php
@@ -1,17 +1,15 @@
 <?php
 
-// Konfigurasi database
-define('DB_HOST', 'localhost'); // Ganti dengan host database Anda
-define('DB_USER', 'root'); // Ganti dengan username database Anda
-define('DB_PASS', ''); // Ganti dengan password database Anda
-define('DB_NAME', 'taskmaster'); // Ganti dengan nama database Anda
+$host = 'localhost';
+$db_name = 'taskmaster';
+$username = 'root';
+$password = '';
 
-// Koneksi ke database
-$db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-// Cek koneksi ke database
-if ($db->connect_errno) {
-    die("Gagal terhubung ke database: " . $db->connect_error);
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 
 ?>
