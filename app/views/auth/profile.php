a034cd7860ff630ae60b1b6a7527cd274b3a9b14
unknown <haikalrafifas@github.com>
2023-04-14 06:00:14 +0700

    Trimmed header and footer since it is already included

--- app/views/auth/profile.php
+++ app/views/auth/profile.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Taskmaster - Profile</title>
-    <!-- Anda dapat menambahkan CSS dan JavaScript di sini jika diperlukan -->
-</head>
-<body>
     <h1>Profile</h1>
     <p>Username: <?php echo $_SESSION['user']['nama']; ?></p>
     <p>Email: <?php echo $_SESSION['user']['email']; ?></p>
 
     <a href="index.php?action=edit_profile">Edit Profile</a>
     <a href="index.php?action=delete_account">Delete Account</a>
-</body>
-</html>
