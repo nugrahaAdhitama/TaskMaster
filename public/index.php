8666b7d1571138b794fe1bc2f54d97e349f3ba09
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-04-13 07:30:30 +0700

    Add logout functionality to index.php
    In this commit, I added the functionality to logout from the website by creating a new case in the switch statement in index.php. The new case 'logout' destroys the session and redirects the user to the index.php page. I also made changes to the default case, which now leads to a welcome page with options to either register or login. This is a significant improvement to the website's authentication system, as users can now log out of their accounts.

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ require '../config/database.php';
 require '../app/models/User.php';
 require '../app/controllers/AuthController.php';
 
+session_start();
+
 $action = isset($_GET['action']) ? $_GET['action'] : 'default';
 
 switch ($action) {
@@ -13,10 +15,27 @@ switch ($action) {
     case 'register_process':
         require_once 'register_process.php';
         break;
+    case 'login':
+        require_once '../app/views/auth/login.php';
+        break;
+    case 'login_process':
+        require_once 'login_process.php';
+        break;
+    case 'dashboard':
+        if (isset($_SESSION['user'])) {
+            require_once '../app/views/dashboard.php';
+        } else {
+            header("Location: index.php?action=login");
+        }
+        break;
+    case 'logout':
+        unset($_SESSION['user']);
+        header("Location: index.php");
+        break;
     // Tambahkan kasus routing lainnya di sini
     default:
-        // Mengarahkan ke halaman register sebagai halaman default
-        require_once '../app/views/auth/register.php';
+        // Mengarahkan ke halaman default dengan pilihan untuk login atau register
+        require_once '../app/views/auth/welcome.php';
 }
 
 ?>
