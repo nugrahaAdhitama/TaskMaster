7e5655c50340d3ae6fa44ea445d6de023686854b
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-04-14 06:47:46 +0700

    Add button for back to dashboard
    This commit add new button which is Back to Dashboard.

--- app/views/auth/profile.php
+++ app/views/auth/profile.php
@@ -4,3 +4,4 @@
 
     <a href="index.php?action=edit_profile">Edit Profile</a>
     <a href="index.php?action=delete_account">Delete Account</a>
+    <a href="index.php?action=dashboard">Back to Dashboard</a>
