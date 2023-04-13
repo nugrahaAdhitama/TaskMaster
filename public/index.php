5f9849be00e9d3a5b9e2cd0a2bda41ca17c442c5
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-04-13 20:43:10 +0700

    Added routing for authentication and user profile features
    This commit adds routing functionality to index.php for various authentication and user profile features, such as registration, login, dashboard, profile display, profile editing, and account deletion. The code uses the AuthController and User classes to handle these features, and includes appropriate redirects when necessary. Templates for the header and footer are also included.

--- public/index.php
+++ public/index.php
@@ -15,15 +15,19 @@ switch ($action) {
     case 'register':
         require_once '../app/views/auth/register.php';
         break;
+
     case 'register_process':
         require_once 'register_process.php';
         break;
+
     case 'login':
         require_once '../app/views/auth/login.php';
         break;
+
     case 'login_process':
         require_once 'login_process.php';
         break;
+
     case 'dashboard':
         if (isset($_SESSION['user'])) {
             require_once '../app/views/dashboard.php';
@@ -31,10 +35,77 @@ switch ($action) {
             header("Location: index.php?action=login");
         }
         break;
+
     case 'logout':
         unset($_SESSION['user']);
         header("Location: index.php");
         break;
+
+    case 'profile':
+        if (isset($_SESSION['user'])) {
+            require_once '../app/views/auth/profile.php';
+        } else {
+            header("Location: index.php?action=profile");
+        }
+        break;
+
+    case 'edit_profile':
+        if (isset($_SESSION['user'])) {
+            require_once '../app/views/auth/edit_profile.php';
+        } else {
+            header("Location: index.php?action=edit_profile");
+        }
+        break;
+
+    case 'edit_profile_process':
+        if (isset($_POST['submit']) && $_POST['submit'] == 'Save') {
+            require_once '../app/controllers/AuthController.php';
+            $authController = new AuthController($conn);
+                
+            $nama = isset($_POST['nama']) ? $_POST['nama'] : '';
+            $password = isset($_POST['password']) ? $_POST['password'] : '';
+                
+            $updated = $authController->updateProfile($_SESSION['user']['email'], $nama, $password);
+            if ($updated) {
+                // Update session data
+                $_SESSION['user']['nama'] = $nama;
+                header('Location: index.php?action=profile');
+                exit;
+            } else {
+                // Jika gagal mengupdate profil, arahkan kembali ke halaman edit profile
+                header('Location: index.php?action=edit_profile');
+                exit;
+            }
+        } else {
+            header('Location: index.php?action=edit_profile');
+            exit;
+        }
+        break;
+
+    case 'delete_account':
+        require_once '../app/views/auth/delete_profile.php';
+        break;
+
+    case 'delete_account_process':
+        if (isset($_POST['submit']) && $_POST['submit'] == 'yes') {
+            require_once '../app/controllers/AuthController.php';
+            $authController = new AuthController($conn);
+            $deleted = $authController->deleteProfile($_SESSION['user']['email']);
+            if ($deleted) {
+                unset($_SESSION['user']);
+                header('Location: index.php?action=login');
+                exit;
+            } else {
+                // Jika gagal menghapus akun, arahkan kembali ke halaman profil
+                header('Location: index.php?action=profile');
+                exit;
+            }
+        } else {
+            header('Location: index.php?action=profile');
+            exit;
+        }
+        break;
+        
     // Tambahkan kasus routing lainnya di sini
     default:
         // Mengarahkan ke halaman default dengan pilihan untuk login atau register
