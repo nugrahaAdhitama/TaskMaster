705e22a7adb8b27948708c6d13fc57d016ac8ede
haikalrafifas <haikalrafifas@github.com>
2023-04-14 06:46:58 +0700

    Trimmed header and footer since it is already included

--- app/views/auth/delete_profile.php
+++ app/views/auth/delete_profile.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Taskmaster - Delete Account</title>
-    <!-- Anda dapat menambahkan CSS dan JavaScript di sini jika diperlukan -->
-</head>
-<body>
     <h1>Delete Account</h1>
     <p>Are you sure you want to delete your account?</p>
 
@@ -14,5 +5,3 @@
         <button type="submit" name="submit" value="yes">Yes</button>
         <button type="submit" name="submit" value="no">No</button>
     </form>
-</body>
-</html>
