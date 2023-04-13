8b86104b951fd4da169958f0000967c592671b5d
unknown <haikalrafifas@github.com>
2023-04-14 06:03:19 +0700

    Trimmed header and footer since it is already included

--- app/views/auth/edit_profile.php
+++ app/views/auth/edit_profile.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Profile - Taskmaster</title>
-    <!-- Anda dapat menambahkan CSS dan JavaScript di sini jika diperlukan -->
-</head>
-<body>
     <h1>Edit Profile</h1>
     <form action="index.php?action=edit_profile_process" method="post">
         <label for="nama">Nama:</label>
@@ -16,5 +7,3 @@
         <input type="submit" value="Save">
     </form>
     <a href="index.php?action=profile">Back to Profile</a>
-</body>
-</html>
