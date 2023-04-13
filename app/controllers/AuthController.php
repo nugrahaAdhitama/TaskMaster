3a0e4992c3ba85b2ba832e28f1f8a769353aa9a7
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-04-13 20:27:40 +0700

    Added AuthController class with register, login, readProfile, updateProfile, and deleteProfile methods
    The AuthController class has been added to handle user authentication and profile management. It includes methods for registering new users, logging in existing users, reading user profiles, updating user profiles, and deleting user accounts. These methods interact with the User model to execute database queries and update user information.

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -11,25 +11,70 @@ class AuthController {
 
     public function register($nama, $email, $password) {
         $user = new User($this->conn);
+        $hashedPassword = $user->hashPassword($password);
     
         if ($user->isEmailRegistered($email)) {
             return false;
         }
     
-        return $user->register($nama, $email, $password);
+        return $user->register($nama, $email, $hashedPassword);
     }
-
+    
     public function isEmailRegistered($email) {
         $user = new User($this->conn);
         return $user->getUserByEmail($email) !== false;
     }
-    
-    
 
     public function login($email, $password) {
         $user = new User($this->conn);
         return $user->login($email, $password);
     }
+
+    public function readProfile($email) {
+        $user = new User($this->conn);
+        return $user->getUserByEmail($email);
+    }
+    
+    public function updateProfile($email, $nama, $password) {
+        $user = new User($this->conn);
+        $user_data = $user->getUserByEmail($email);
+        if ($user_data === false) {
+            return false;
+        }
+    
+        if (!empty($nama)) {
+            $user_data['nama'] = $nama;
+        }
+    
+        if (!empty($password)) {
+            $user_data['password'] = $user->hashPassword($password);
+        }
+    
+        $query = "UPDATE users SET nama = :nama, password = :password WHERE email = :email";
+        $stmt = $this->conn->prepare($query);
+    
+        $stmt->bindParam(':nama', $user_data['nama']);
+        $stmt->bindParam(':password', $user_data['password']);
+        $stmt->bindParam(':email', $email);
+    
+        return $stmt->execute();
+    }
+    
+    public function deleteProfile($email) {
+        $user = new User($this->conn);
+        $user_data = $user->getUserByEmail($email);
+        if ($user_data === false) {
+            return false;
+        }
+    
+        $query = "DELETE FROM users WHERE email = :email";
+        $stmt = $this->conn->prepare($query);
+    
+        $stmt->bindParam(':email', $email);
+    
+        return $stmt->execute();
+    }
+    
 }
 
 ?>
