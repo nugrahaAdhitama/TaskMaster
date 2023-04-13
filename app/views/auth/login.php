8d6be848e4ed73d7a36bed019813d685991aada3
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-04-13 07:23:13 +0700

    Create login view in app/views/auth/login.php
    This commit adds a new login view in the app/views/auth directory. The login.php file contains the HTML form for users to input their email and password. It will be used for the user authentication process in the Taskmaster application.

---
+++ app/views/auth/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taskmaster - Login</title>
+    <!-- Anda dapat menambahkan CSS dan JavaScript di sini jika diperlukan -->
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="index.php?action=login_process" method="post">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
